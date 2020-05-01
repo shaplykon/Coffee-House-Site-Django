1c59d9ed586c25bbb2854575250471b6fb9c3d99
shaplykon <62645506+shaplykon@users.noreply.github.com>
2020-05-02 00:48:30 +0300

    Add files via upload

--- coffee_house/urls.py
+++ coffee_house/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include, re_path
 from django.conf import settings
 
 urlpatterns = [
+    re_path(r'^admin/', include(admin.site.urls)),
     re_path(r'^$', include('index.urls')),
     re_path(r'menu/', include('menu.urls')),
     re_path(r'post/', include('article.urls')),
