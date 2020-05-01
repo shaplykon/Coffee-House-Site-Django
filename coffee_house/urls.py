eb8fd13351f7f44dac950dfb2e79cc5663292aa6
shaplykon <62645506+shaplykon@users.noreply.github.com>
2020-05-01 22:18:54 +0300

    Add files via upload

--- coffee_house/urls.py
+++ coffee_house/urls.py
@@ -2,13 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^$', include('index.urls')),
     re_path(r'menu/', include('menu.urls')),
     re_path(r'post/', include('article.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
